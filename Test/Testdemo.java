5bab4eb418cbfab4da3487a87191ecbfe29eafb7
Twelve <157513646@qq.com>
2020-04-09 14:58:48 +0800

    java Testdemo code

---
+++ Test/Testdemo.java
@@ -0,0 +1,56 @@
+public class Testdemo{
+                 public static void main(String [] args) {
+                /*  int num = 10; 
+				 System.out.println(num) ;  */
+				/*  for(int i = 0; i< args.length ; i++) {
+					 System.out.println(args[i]);
+                 }
+				 System.out.println("ceshi"); */
+				 /* long num = 10L; 
+				 System.out.println(num) ;  */
+				 /* double a = 1.0; 
+				 double b = 2.0; 
+				 double num = 1.1; 
+				 System.out.println(a / b);
+				 System.out.println(num * num); */
+				/*  float num = 1.0f;  
+				 System.out.println(num); */
+				/*  char ch = '呵'; 
+				 System.out.println(ch); */
+				/*  byte value = 0; 
+				 System.out.println(value); */
+				 /* short value = 0; 
+				 System.out.println(value); */
+				/*  boolean value = true;
+				 System.out.println(value); */
+				/*  String name = "Twelve";
+				 System.out.println(name); */
+				/*  String a = "hello";
+				 String b = "world"; 
+				 String c = a + b; 
+				 System.out.println(c); */
+				/*  String str = "result = "; 
+				 int a = 10; 
+				 int b = 20;
+				 String result = str + a + b;
+				 System.out.println(result); */
+				/*  int a = 10; 
+				 long b = 20; 
+			     long d = a + b;
+                 System.out.println(d);	 */	
+                 /* byte a = 10;
+				 byte b = 20; 
+				 byte c = (byte)(a + b); 
+				 System.out.println(c);	 */	
+				 String str = "100"; 
+				 int num = Integer.parseInt(str); 
+				  System.out.println(num);	
+				 }
+  } 
+  
+  
+  
+  
+  
+  
+  

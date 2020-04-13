16745460bc821b64f386feb78c54a3b5239a6b06
Twelve <157513646@qq.com>
2020-04-13 19:32:55 +0800

    pratice code

---
+++ javacode/2020-4-8/Testdemo.java
@@ -0,0 +1,6 @@
+public class Testdemo{
+                 public static void main(String [] args){
+                     char ch = '呵';
+                     System.out.println(ch);
+                 }
+  } 
